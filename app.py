4f280f356cffc3ddc0b2008bb793e13bd68657de
abhay <khakhriyaabhay073@gmail.com>
2026-07-15 17:40:53 +0530

    Refactor app routes and templates: implement home and movies pages, add footer, and clean up unused code

--- app.py
+++ app.py
@@ -1,46 +1,16 @@
-from flask import Flask , request,url_for,redirect,session,Response
+from flask import Flask , request,url_for,redirect,session,Response , render_template
 
 app = Flask(__name__)
-app.secret_key = 	
+app.secret_key = "your_secret_key"  # Replace with your own secret key
 
-# login page
-@app.route('/',methods=["GET","POST"])
-def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-        if username == "Abhay" and password == "1504":
-            session["user"] = username #sore user name
-            return redirect(url_for("welcome"))
-        else:
-            return Response("In-valid content. Try again", mimetype="text/plain") #text/HTML
-        
-    return'''
+@app.route('/movies')
+def movies():
+    return render_template("movie.html")
 
-            <h2> Login Page </h2>
-            <form method="POST">
-            Username: <input type="text" name="username"><br>
-            Password:<input type="password" name="password"><br>
-            <input type="submit" value="Login">
-            </form>
+if __name__ == '__main__':
+    app.run(debug=True)
 
-'''
-
-# welcome page
-
-@app.route("/welcome")
-def welcome():
-    if "user" in session:
-        return f'''
-        <h2> Welcome , {session["user"]}</h2>
-        <a href={url_for('logout')}>Logout</a>
-    '''
-    return redirect(url_for("login"))
-
-# logout
-
-@app.route("/logout")
-def logout():
-    session.pop("user",None)
-    return redirect(url_for("login"))
